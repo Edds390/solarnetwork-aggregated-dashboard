ce427ac9597cec324481656b6a4ac245ad6fbc00
Edds <veryshortemailaddress@gmail.com>
2018-03-23 06:10:05 +0000

    Feature/prototype searchbar (#7)
    * Changed eslint rules to account for browser environment
    
    * updated readme to clearly indicate which branch name you should be creating.
    
    * Made skeleton for the landing page.

---
+++ src/components/Autocomplete/Autocomplete.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import AutoComplete from 'material-ui/AutoComplete';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+/**
+ * `AutoComplete` search text can be implemented as a controlled value,
+ * where `searchText` is handled by state in the parent component.
+ * This value is reset with the `onNewRequest` callback.
+ */
+export default class AutoCompleteExampleControlled extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+    };
+  }
+
+  handleSelect = (searchText) => {
+    const { onSearch } = this.props;
+    onSearch(searchText);
+  }
+
+  handleUpdateInput = (searchText) => {
+    
+    this.setState({
+      searchText,
+    });
+  };
+
+  handleNewRequest = () => {
+    this.setState({
+      searchText: '',
+    });
+  };
+
+  render() {
+    const {
+      suggestionList,
+    } = this.props;
+    return (
+      <MuiThemeProvider>
+        <AutoComplete
+          hintText="Project..."
+          searchText={this.state.searchText}
+          onNewRequest={this.handleSelect}
+          onUpdateInput={this.handleUpdateInput}
+          dataSource={suggestionList}
+          filter={(searchText, key) => (key.indexOf(searchText) !== -1)}
+          openOnFocus
+          fullWidth
+          textFieldStyle={{ textAlign: 'center', color: 'blue' }}
+        />
+      </MuiThemeProvider>
+    );
+  }
+}
