cb86fb0c41541f9f8f3cb7550e383927e7c7ce9b
Edison Rho <veryshortemailaddress@gmail.com>
2018-03-24 14:23:25 +1300

    Added data source example

--- src/components/MasterChart/MasterChart.js
+++ src/components/MasterChart/MasterChart.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
 import { AreaChart, Area, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
 
-const data = [
-  {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-  {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-  {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-  {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-  {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-  {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-  {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
+// const data = [
+//   {name: new Date(), uv: 4000, pv: 2400, amt: 2400},
+//   {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+//   {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+//   {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+//   {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+//   {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+//   {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+// ];
 const labels = {
   labels: ["x", "A", "B"],
 };
@@ -23,7 +23,30 @@ export default class MasterChart extends Component {
     };
   }
 
+  makeSeries = () => {
+    const startDate = new Date();
+    const length = 30;
+    const max = 100;
+    const data = [];
+    let i = 0;
+    // const max = Math.random() > 0.5 ? 100000 : 10
+    // const multiplier = 10
+    // const multiplier = Math.round((Math.random() * 10) + Math.round(Math.random() * 50))
+    for (i = 0; i < length; i += 1) {
+      startDate.setDate(startDate.getDay() + 1);
+      const dayNum = startDate.getDate();
+      data.push({
+        name: dayNum,
+        uv: Math.round(Math.random() + Math.round(Math.random())),
+        pv: Math.round(Math.random() * 5),
+        amt: Math.round(Math.random()),
+      });
+    }
+    return data;
+  }
+
   render() {
+    const data = this.makeSeries();
     return (
       <Paper className="paper">
         <AreaChart width={600} height={400} data={data}
