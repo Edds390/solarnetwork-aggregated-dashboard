3d74476a5ced37fa46778b6947fb415742498a03
Eli Salter <esal284@aucklanduni.ac.nz>
2018-03-25 14:45:07 +1300

    added basic API structure

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+const PREFIX = '/solarquery';
+
+const queries = {
+  // This method returns information about the span of time data is available for a particular node.
+
+};
+
+export default queries;
