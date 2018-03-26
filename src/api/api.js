7906aacd7c3330aafa28d47b1ba5c51610dc1a3b
Seoyoung Choi <seoyoungc@gmail.com>
2018-03-26 15:53:56 +1300

    Changed API call to use async/await

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,15 @@
 const HOST = 'https://data.solarnetwork.net';
 
 // This method returns usage data for a single node between a set time
-const getAllUsageData = (nodeId, startDate, endDate) => {
-  const query = `https://data.solarnetwork.net/solarquery/api/v1/pub/datum/list?nodeId=${nodeId}&startDate=${startDate}T0%3A19&endDate=${endDate}T0%3A19&aggregate=Hour`;
-  fetch(query)
-    .then(results => results.json())
-    .then((json) => {
-      return json;
-    });
+const getNodeUsageData = async (nodeId, startDate, endDate) => {
+  const query = `https://data.solarnetwork.net/solarquery/api/v1/pub/datum/list?nodeId=${nodeId}&startDate=${startDate}T0%3A19&endDate=${endDate}T0%3A19&aggregate=Hour`; 
+  try {
+    const results = await fetch(query);
+    const json = await results.json();
+    return json;
+  } catch (err) {
+    return err;
+  }
 };
 
-export default getAllUsageData;
+export default getNodeUsageData;
