30faaa13fd679e8cfbb6eb3dcd65ca9502d597f8
Sejal <spt098@aucklanduni.ac.nz>
2018-03-28 00:00:58 +1300

    Formatted & Commented the Code.

--- src/components/DataTable/PanelCards/PanelSet.js
+++ src/components/DataTable/PanelCards/PanelSet.js
@@ -1,77 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Panel, PanelGroup, Button } from 'react-bootstrap';
+import { Panel, PanelGroup, Button } from 'react-bootstrap';
 
-const nodeMap = [
-  {
-    title: 'Node 263',
-    minPower: 288,
-    maxPower: 465,
-  },
-  {
-    title: 'Node 5',
-    minPower: 123,
-    maxPower: 354,
-  },
-  {
-    title: 'Node 135',
-    minPower: 7,
-    maxPower: 45,
-  },
-];
-
+/* A new component created for the node data table, due to the initial one causing overlap with the
+graph. Used the bootstrap library for this implementation */
 export default class PanelSet extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: 1,
-      selectedNodes: new Set() };
+    this.state = {
+      selectedNodes: new Set(),
+    };
   }
-    
-  
+
+  /* By using the selectedNodes prop obtained from the DashBoardPanel component,
+this functionality filters through the nodes and selects the one that are true
+(were selected by the user). Once there is a filtered list of nodes, each panel is created to
+display information associated with their corresponding node */
   render() {
-const { checklistToggleMap } = this.props;
+    const { checklistToggleMap } = this.props;
+    const chosenNodes = new Set(this.state.selectedNodes);
 
-console.log(checklistToggleMap);
-const chosenNodes = new Set(this.state.selectedNodes);
-Object.keys(checklistToggleMap).forEach((node) => {
-  if (checklistToggleMap[node]) {
-    chosenNodes.add(node);
-  }
-});
-console.log(chosenNodes);
-const array = Array.from(chosenNodes);
+    Object.keys(checklistToggleMap).forEach((node) => {
+      if (checklistToggleMap[node]) {
+        chosenNodes.add(node);
+      }
+    });
 
-const nodeCards = array.map((nodeId,i) => (
-  <Panel eventKey={i} >
-  <Panel.Heading>
-<Panel.Title >{nodeId}</Panel.Title>
-</Panel.Heading>
+    const array = Array.from(chosenNodes);
+
+    const nodeCards = array.map((nodeId, i) => (
+      <Panel eventKey={i} >
+        <Panel.Heading>
+          <Panel.Title >{nodeId}</Panel.Title>
+        </Panel.Heading>
         <Panel.Body >
-        <p>Minimum Power: ... Watts.</p>
-      <p>Maximum Power: ... Watts</p>
-</Panel.Body>
-</Panel>));
+          <p>Minimum Power: ... Watts.</p>
+          <p>Maximum Power: ... Watts</p>
+        </Panel.Body>
+      </Panel>));
     return (
 
-     <div>
-     <Button onClick={() => this.setState({ open: !this.state.open })}>
+      <div>
+        <Button onClick={() => this.setState({ open: !this.state.open })}>
           View Node Information
         </Button>
         <br />
         <Panel id="collapsible-panel-example-1" expanded={this.state.open}>
           <Panel.Collapse>
             <Panel.Body>
-             <PanelGroup>{nodeCards}
-                 </PanelGroup>
+              <PanelGroup>{nodeCards}
+              </PanelGroup>
             </Panel.Body>
           </Panel.Collapse>
         </Panel>
-        </div>
+      </div>
     );
   }
 }
 
 PanelSet.propTypes = {
-  checklistToggleMap: PropTypes.object.isRequired
+  checklistToggleMap: PropTypes.object.isRequired,
 };
