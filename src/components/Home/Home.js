5ce45a57709b58ab3d0af4cdd993bbcf9f0bd024
Kenney Chan <Kcha582@aucklanduni.ac.nz>
2018-03-27 14:39:19 +1300

    Fixed the problem where the image will not scale to the window size

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,7 @@ import cloneDeep from 'lodash';
 import { Link } from 'react-router-dom';
 import Autocomplete from '../Autocomplete/Autocomplete';
 import TopNavigationBar from '../TopNavigationBar/TopNavigationBar';
+import background from '../../icons/background.png';
 
 
 import './Home.css';
@@ -79,6 +80,7 @@ export default class Home extends Component {
             {nodeChips}
           </div>
         </Paper>
+        <img class="background" id="background1" src={background}/>
       </div>
     );
   }
